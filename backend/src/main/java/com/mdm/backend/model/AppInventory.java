cecd521decde3b1ea4a13464b2eb959bce84a7b2
Sujan-2006 <sujan032006@gmail.com>
2026-03-11 11:27:57 +0530

    appinventory fixed

--- backend/src/main/java/com/mdm/backend/model/AppInventory.java
+++ backend/src/main/java/com/mdm/backend/model/AppInventory.java
@@ -1,5 +1,6 @@
 package com.mdm.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +21,10 @@ public class AppInventory {
     private String packageName;
     private String versionName;
     private int versionCode;
+
+    @JsonProperty("isSystemApp")
+    @Column(name = "is_system_app")
     private Boolean isSystemApp;
+
     private String installSource;
 }
